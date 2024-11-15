b406bcffe6871bd8d86f85e7efd33992106c12f6
Toth-Mate <t0thm4t3@gmail.com>
2024-11-15 10:26:19 +0100

    StudentDto osztály

---
+++ Kreata.Shared/Dtos/StudentDto.cs
@@ -0,0 +1,28 @@
+﻿using Kreata.Shared.Models.Enums;
+
+namespace Kreata.Shared.Dtos
+{
+    public class StudentDto
+    {
+        public StudentDto(Guid id, string firstName, string lastName, DateTime birthsDay, int schoolYear, SchoolClassType schoolClass, string educationLevel, bool isWoomen)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthsDay = birthsDay;
+            SchoolYear = schoolYear;
+            SchoolClass = schoolClass;
+            EducationLevel = educationLevel;
+            IsWoomen = isWoomen;
+        }
+
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthsDay { get; set; }
+        public int SchoolYear { get; set; }
+        public SchoolClassType SchoolClass { get; set; }
+        public string EducationLevel { get; set; }
+        public bool IsWoomen { get; set; }
+    }
+}

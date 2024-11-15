2d0bf7507e5866b7a8aee54c41cb8ad183872143
Toth-Mate <t0thm4t3@gmail.com>
2024-11-15 11:00:13 +0100

    TeacherRepo (+interfész)

---
+++ Kreata.Backend/Repos/ITeacherRepo.cs
@@ -0,0 +1,12 @@
+﻿using Kreata.Shared.Models.Entities;
+using Kreata.Shared.Models.Responses;
+
+namespace Kreata.Backend.Repos
+{
+    public interface ITeacherRepo
+    {
+        Task<List<Teacher>> GetAll();
+        Task<Teacher?> GetBy(Guid id);
+        Task<ControllerResponse> UpdateTeacherAsync(Teacher teacher);
+    }
+}
---
+++ Kreata.Backend/Repos/TeacherRepo.cs
@@ -0,0 +1,45 @@
+﻿using Kreata.Backend.Context;
+using Kreata.Shared.Models.Entities;
+using Kreata.Shared.Models.Responses;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kreata.Backend.Repos
+{
+    public class TeacherRepo : ITeacherRepo
+    {
+        private readonly KretaInMemoryContext _dbContext;
+
+        public TeacherRepo(KretaInMemoryContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Teacher>> GetAll()
+        {
+            return await _dbContext.Teachers.ToListAsync();
+        }
+
+        public Task<Teacher?> GetBy(Guid id)
+        {
+            return await _dbContext.Teachers.FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<ControllerResponse> UpdateTeacherAsync(Teacher teacher)
+        {
+            ControllerResponse response = new ControllerResponse();
+            _dbContext.ChangeTracker.Clear();
+            _dbContext.Entry(teacher).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch(Exception e)
+            {
+                response.AppendNewError(e.Message);
+                response.AppendNewError($"{nameof(TeacherRepo)} osztály, {nameof(UpdateTeacherAsync)} metódusában hiba.");
+                response.AppendNewError($"{teacher} frissítése nem sikerült.");
+            }
+        }
+    }
+}

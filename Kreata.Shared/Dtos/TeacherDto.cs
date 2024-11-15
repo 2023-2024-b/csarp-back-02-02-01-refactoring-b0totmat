f688a6a1643ef3cdc0a0a6cdf8671c5789e36e31
Toth-Mate <t0thm4t3@gmail.com>
2024-11-15 10:43:18 +0100

    Teacher model és Dto osztályok

---
+++ Kreata.Shared/Dtos/TeacherDto.cs
@@ -0,0 +1,30 @@
+﻿namespace Kreata.Shared.Dtos
+{
+    public class TeacherDto
+    {
+        public TeacherDto()
+        {
+        }
+
+        public TeacherDto(Guid id, string firstName, string lastName, DateTime birthDay, string placeOfBirth, bool isWoman, bool isHeadTeacher, string mathersName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDay = birthDay;
+            PlaceOfBirth = placeOfBirth;
+            IsWoman = isWoman;
+            IsHeadTeacher = isHeadTeacher;
+            MathersName = mathersName;
+        }
+
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string PlaceOfBirth { get; set; }
+        public bool IsWoman { get; set; }
+        public bool IsHeadTeacher { get; set; }
+        public string MathersName { get; set; }
+    }
+}

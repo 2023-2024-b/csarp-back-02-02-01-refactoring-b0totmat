8eac100d643f4fb32cbc5b10263f5a8e174e2163
Toth-Mate <t0thm4t3@gmail.com>
2024-11-15 10:30:04 +0100

    StudentExtension osztály -> ToStudentDto() metódus

---
+++ Kreata.Shared/Extensions/StudentExtension.cs
@@ -0,0 +1,23 @@
+﻿using Kreata.Shared.Dtos;
+using Kreata.Shared.Models.Entities;
+
+namespace Kreata.Shared.Extensions
+{
+    public static class StudentExtension
+    {
+        public static StudentDto ToStudentDto(this Student student)
+        {
+            return new StudentDto
+            {
+                Id = student.Id,
+                FirstName = student.FirstName,
+                LastName = student.LastName,
+                BirthsDay = student.BirthsDay,
+                SchoolYear = student.SchoolYear,
+                SchoolClass = student.SchoolClass,
+                EducationLevel = student.EducationLevel
+            };
+        }
+
+    }
+}
